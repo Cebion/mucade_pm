c17dd7bd4f59e3ea1a45b6d4bf7190b03c83d612
root <7762208+Cebion@users.noreply.github.com>
2025-03-26 15:57:33 +0000

    Change libbullet linker flag, lib location

--- sources/buildLinux.sh
+++ sources/buildLinux.sh
@@ -15,33 +15,34 @@ rm src/abagames/util/sdl/*.o*
 rm src/abagames/mcd/*.o*
 
 cd import
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS {}
 cd sdl
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS {}
 cd ../bindbc/sdl
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS {}
 cd ../../ode
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS -I.. \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS -I.. {}
 cd ../..
 
 cd src/abagames/util
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS -I../.. \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS -I../.. {}
 cd ../../..
 
 cd src/abagames/util/bulletml
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS -I../../.. \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS -I../../.. {}
 cd ../../../..
 
 cd src/abagames/util/ode
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS -I../../.. \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS -I../../.. {}
 cd ../../../..
 
 cd src/abagames/util/sdl
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS -I../../.. \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS -I../../.. {}
 cd ../../../..
 
 cd src/abagames/mcd
-find . -maxdepth 1 -name \*.d -type f -exec gdc $FLAGS -I../.. \{\} \;
+find . -maxdepth 1 -name "*.d" -type f | xargs -P 16 -I{} gdc $FLAGS -I../.. {}
 cd ../../..
 
-gdc -o Mu-cade -s -Wl,--gc-sections -static-libphobos import/*.o* import/ode/*.o* import/sdl/*.o* import/bindbc/sdl/*.o* src/abagames/util/*.o* src/abagames/util/bulletml/*.o* src/abagames/util/ode/*.o* src/abagames/util/sdl/*.o* src/abagames/mcd/*.o* -lGLU -lGL -lSDL2_mixer -lSDL2 -lbulletml_d -lode
+# Changed to use libbulletml.so instead of libbulletml_d.a based on symbol verification
+gdc -o Mu-cade -s -Wl,--gc-sections -static-libphobos import/*.o* import/ode/*.o* import/sdl/*.o* import/bindbc/sdl/*.o* src/abagames/util/*.o* src/abagames/util/bulletml/*.o* src/abagames/util/ode/*.o* src/abagames/util/sdl/*.o* src/abagames/mcd/*.o* -lGLU -lGL -lSDL2_mixer -lSDL2 -lbulletml -lode
